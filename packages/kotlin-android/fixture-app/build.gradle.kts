578efc7a2514a5301ab69846af5f0d033d0a6ed8
Jaden Fix <jadenfix20@gmail.com>
2026-09-08 00:08:01 -0700

    Resolve Android fixture plugins from the root build

--- packages/kotlin-android/fixture-app/build.gradle.kts
+++ packages/kotlin-android/fixture-app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.9.2"
-    id("org.jetbrains.kotlin.android") version "2.1.20"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
